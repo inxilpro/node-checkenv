851f723bf4bbac4b726c018ae4487891d2df5d96
Chris Morrell <inxilpro@users.noreply.github.com>
2015-12-03 13:03:15 -0500

    Additional testing

--- test/optional/index.js
+++ test/optional/index.js
@@ -4,19 +4,12 @@ var tape = require('tape');
 var checkenv = require('../../' + process.argv[2] + '/index');
 
 // Shims
-var _exit = process.exit;
 var _err = console.error;
-process.exit = function(code) { exited = true; lastcode = code; };
 console.error = function(m) { errored++ };
 
-var exited = false;
 var errored = 0;
-var lastcode;
-
 function reset(pass) {
-	exited = false;
 	errored = 0;
-	lastcode = null;
 
 	if (pass) {
 		process.env.A = true;
@@ -32,32 +25,29 @@ function reset(pass) {
 }
 
 tape('check()', function(t) {
-	t.plan(5);
+	t.plan(2);
 
 	reset();
 	checkenv.check();
-	t.equal(exited, true, 'should call process.exit() if required variables are missing');
-	t.ok(lastcode > 0, 'should exit with a non-zero exit code if process.exit() is called');
-	t.ok(errored > 0, 'should call console.error() if required variables are missing');
+	t.ok(errored > 0, 'should call console.error() if optional variables are missing');
 
 	reset(true);
 	checkenv.check();
-	t.equal(exited, false, 'should not call process.exit() if required variables are set');
-	t.equal(errored, 0, 'should not call console.error() if required variables are set');
+	t.equal(errored, 0, 'should not call console.error() if all variables are set');
 });
 
 tape('check(false)', function(t) {
 	t.plan(4);
 
 	reset();
-	t.throws(function() {
+	t.doesNotThrow(function() {
 		checkenv.check(false);
-	}, 'should throw an error if required variables are missing');
+	}, 'should not throw an error if optional variables are missing');
 	t.equal(errored, 0, 'and should not call console.error()');
 
 	reset(true);
 	t.doesNotThrow(function() {
 		checkenv.check(false);
-	}, 'should not throw an error if required variables are set');
+	}, 'should not throw an error if optional variables are set');
 	t.equal(errored, 0, 'and should not call console.error()');
 });
