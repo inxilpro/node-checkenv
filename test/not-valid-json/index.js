402cca27b4dc420147540a81d6cf08ebe8fb550e
yalcindo <yalcindo1@gmail.com>
2017-03-29 21:16:17 -0400

    Add more tests

--- test/not-valid-json/index.js
+++ test/not-valid-json/index.js
@@ -6,9 +6,23 @@ var spy = require('../spy.js');
 
 tape('WHEN env.json IS  NOT VALID JSON:', function(s) {
 	s.test('check()', function(t) {
-		t.plan(1);
+		t.plan(4);
+		spy.setup();
+
+		spy.reset();
+
 		t.throws(function() {
 			checkenv.check();
 		}, /SyntaxError/i, 'should throw a "SyntaxError" error');
+		t.equal(spy.errorCount(), 0, 'and should not call console.error()');
+
+		spy.reset(['A']);
+
+		t.throws(function() {
+			checkenv.check(false);
+		}, /SyntaxError/i, 'should throw a "SyntaxError" error');
+		t.equal(spy.errorCount(), 0, 'and should not call console.error()');
+
+		spy.restore();
 	});
 });
